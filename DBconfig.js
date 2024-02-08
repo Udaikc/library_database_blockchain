16e1c547cec76276221f5b501d6c86aecf88e706
udaikc <udaikcsps@gmail.com>
2024-02-08 18:37:23 +0530

    form validation complete

--- DBconfig.js
+++ DBconfig.js
@@ -6,7 +6,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const connectionstring=`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const pool=new pool({
+const pool=new Pool({
     connectionstring:isProduction ? process.env.DATABASE_URL : connectionstring
 });
 
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const {pool}=require("./DBconfig");
+const bodyParser = require('body-parser');
+const bcrypt=require("bcrypt");
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.port || 4000;
 
 app.set("view engine","ejs");
@@ -16,6 +20,27 @@ app.get('/user/login',(req,res)=>{
 
 app.get('/user/register',(req,res)=>{
   res.render("register");
+});
+
+app.post('/user/register',(req,res)=>{
+  let {name,email,usn,password}=req.body;
+  console.log("Request body:", req.body);
+  console.log("Password:", password);
+  let errors=[];
+
+if(!name || !email|| !usn || !password){
+  errors.push({message:"please enter all fields"});
+}
+
+if(password.length<6){
+  errors.push({message:"enter the strong password"})
+}
+
+if(errors.length>0){
+  res.render("register",{errors})
+}else{
+  const hashedpassword = await.bcrypt(password,10);
+}
 });
 
 app.get('/user/dashboard',(req,res)=>{
