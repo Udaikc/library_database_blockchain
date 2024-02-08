f5e4f52d72678bf4c79c38ad0afebc8d12bc2410
udaikc <udaikcsps@gmail.com>
2024-02-08 20:01:19 +0530

    connected to database and authentication completed

--- DBconfig.js
+++ DBconfig.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
-const {Pool}=require("pg");
+const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const connectionstring=`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-const pool=new Pool({
-    connectionstring:isProduction ? process.env.DATABASE_URL : connectionstring
+const pool = new Pool({
+    user: 'admin',
+    host: 'localhost',
+    database: 'login',
+    password: 'admin',
+    port: 5432, // Or your database port
 });
 
-module.export={pool};
+module.exports = { pool };
--- index.js
+++ index.js
@@ -1,54 +1,88 @@
 const express = require('express');
 const app = express();
-const {pool}=require("./DBconfig");
+const { pool } = require("./DBconfig");
 const bodyParser = require('body-parser');
-const bcrypt=require("bcrypt");
+const bcrypt = require("bcrypt");
+const flash = require("express-flash");
+const session = require("express-session");
+
+console.log(pool);
+
+app.use(session({
+  secret: "secret",
+
+  resave: false,
+
+  saveUninitialized: false
+}
+
+));
+
+app.use(flash());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = process.env.port || 4000;
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.render("index");
 });
 
-app.get('/user/login',(req,res)=>{
-    res.render("login");
+app.get('/user/login', (req, res) => {
+  res.render("login");
 });
 
-app.get('/user/register',(req,res)=>{
+app.get('/user/register', (req, res) => {
   res.render("register");
 });
 
-app.post('/user/register',(req,res)=>{
-  let {name,email,usn,password}=req.body;
+app.post('/user/register', async (req, res) => {
+  let { name, email, usn, password } = req.body;
   console.log("Request body:", req.body);
   console.log("Password:", password);
-  let errors=[];
+  let errors = [];
 
-if(!name || !email|| !usn || !password){
-  errors.push({message:"please enter all fields"});
-}
+  if (!name || !email || !usn || !password) {
+    errors.push({ message: "please enter all fields" });
+  }
 
-if(password.length<6){
-  errors.push({message:"enter the strong password"})
-}
+  if (password.length < 6) {
+    errors.push({ message: "enter the strong password" })
+  }
 
-if(errors.length>0){
-  res.render("register",{errors})
-}else{
-  const hashedpassword = await.bcrypt(password,10);
-}
+  if (errors.length > 0) {
+    res.render("register", { errors })
+  } else {
+    const hashedpassword = await bcrypt.hash(password, 10);
+    console.log(hashedpassword);
+
+    pool.query(
+      `SELECT * FROM lib_user WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.log("reached here");
+        console.log(results.rows);
+        if (results.rows.length > 0) {
+          errors.push({ message: "user already registered" });
+          res.render("register", { errors })
+        }
+      }
+    );
+
+  }
 });
 
-app.get('/user/dashboard',(req,res)=>{
-  res.render("dashboard",{user:"udai"});
+app.get('/user/dashboard', (req, res) => {
+  res.render("dashboard", { user: "udai" });
 });
 
 
 
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
   console.log(`connected to ${port}`);
 })
